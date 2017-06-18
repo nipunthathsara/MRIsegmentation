6e5d4dfc527dd3aa6264e15fa6486e2a0ed59778
unknown <nipunthathsara@gmail.com>
2017-06-18 20:09:56 +0530

    t1 gray matter segments wel. good for a release

--- main.py
+++ main.py
@@ -276,17 +276,39 @@ class Segmentor (tk.Frame):
                     (205, 125, self.sliceNumber),
                     (173, 205, self.sliceNumber)]
 
-        seedMarkedImg = SimpleITK.Image(self.image)
+        seedMarkedImg = SimpleITK.Image(self.image)#creates a copy
 
         for seed in self.seeds:
-            seedMarkedImg[seed] = 10000
+            seedMarkedImg[seed] = 15000
         Segmentor.tvFrame = tk.Frame(self)
         Segmentor.tvFrame.grid(row=0, column=0, columnspan=4, rowspan=5)
         help.show(seedMarkedImg[:, :, self.sliceNumber], Segmentor.tvFrame)
 
     def onClickSegement(self, mod, matter):
         print('segmenting' +mod+ ' ' + matter)
+        grayMatter = SimpleITK.ConfidenceConnected(image1=self.t1_smoothned,
+                                                        seedList=self.seeds,
+                                                        numberOfIterations=3,
+                                                        multiplier=1, #width of confidence interval,  confidence interval is the mean plus or minus the "Multiplier" times the standard deviation
+                                                        replaceValue=self.grayLabel)
+        """"This filter extracts a connected set of pixels whose pixel intensities are consistent
+        with the pixel statistics of a seed point. The mean and variance across a neighborhood (8-connected, 26-connected, etc.)
+        are calculated for a seed point. Then pixels connected to this seed point whose values are within the confidence interval
+        for the seed point are grouped. The width of the confidence interval is controlled by the "Multiplier" variable
+        (the confidence interval is the mean plus or minus the "Multiplier" times the standard deviation). If the intensity
+        variations across a segment were gaussian, a "Multiplier" setting of 2.5 would define a confidence interval wide enough
+        to capture 99% of samples in the segment.
+        After this initial segmentation is calculated, the mean and variance are re-calculated. All the pixels in the
+        previous segmentation are used to calculate the mean the standard deviation (as opposed to using the pixels in
+        the neighborhood of the seed point). The segmentation is then recalculated using these refined estimates for the
+        mean and variance of the pixel values. This process is repeated for the specified number of iterations.
+         Setting the "NumberOfIterations" to zero stops the algorithm after the initial segmentation from the seed point."""
 
+        #creating rescaled t1_smoothed image. to overlap with graymatter.
+        t1_smoothnedScaled = SimpleITK.Cast(SimpleITK.RescaleIntensity(self.t1_smoothned), grayMatter.GetPixelID())
+        Segmentor.tvFrame = tk.Frame(self)
+        Segmentor.tvFrame.grid(row=0, column=0, columnspan=4, rowspan=5)
+        help.show(SimpleITK.LabelOverlay(t1_smoothnedScaled[:,:,self.sliceNumber], grayMatter[:,:,self.sliceNumber]), Segmentor.tvFrame)
 
 
 
